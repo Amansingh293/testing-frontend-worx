230a612c42e842956518a65162c27313d270c49c
Aman Singh <amansinghdl9c@gmail.com>
2025-03-21 17:36:52 +0530

    integration of new design

--- src/components/CalendlyEmbed.jsx
+++ src/components/CalendlyEmbed.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 
 const CalendlyEmbed = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
     script.async = true;
     document.body.appendChild(script);
 
@@ -14,10 +14,10 @@ const CalendlyEmbed = () => {
 
   return (
     <div
-    className="calendly-inline-widget"
-    data-url="https://calendly.com/salesworx-ai?hide_landing_page_details=1&hide_gdpr_banner=1&primary_color=207c97"
-    style={{ minWidth: '320px', height: '700px' }}
-  />
+      className="calendly-inline-widget"
+      data-url="https://calendly.com/salesworx-ai/demo?hide_gdpr_banner=1&primary_color=207c97"
+      style={{ minWidth: "320px", height: "700px" }}
+    />
   );
 };
 
